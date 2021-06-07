74e5a9f3d79b66e09c8df6078a86c36258c23876
Ghoats <d.beard1@unimail.derby.ac.uk>
2021-06-07 01:06:53 +0100

    Optimised generation through primary broadphase check.

--- PoissonDiscDistributionGenerator.cs
+++ PoissonDiscDistributionGenerator.cs
@@ -1,27 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class PoissonDiscDistributionGenerator
 {
-    private static int MAX_ATTEMPTS = 30;
+    private static int MAX_ATTEMPTS = 3;
 
     /// <summary>
     /// Generates an even Poisson Distribution of points on a disc, with a max radius and minimum spread of points
     /// https://en.wikipedia.org/wiki/Poisson_distribution
     /// </summary>
-    public static List<Vector3> GeneratePoints(ref List<Vector3> points, float spread, float maxRadius)
+    public static void GeneratePoints(List<Vector2> points, float spread, float maxRadius, int seed)
     {
+        Random.InitState(seed);
+        
         points.Clear();
+
+        Debug.Log("Cleared points");
+        int cellSize = Mathf.CeilToInt((maxRadius * 2) / spread);
+        Vector2[,] grid = new Vector2[cellSize, cellSize];
 
         List<Vector2> newPoints = new List<Vector2>();
 
         Vector2 initialPoint = Random.insideUnitCircle;
-        Vector3 newPoint = new Vector3();
+        Vector2 newPoint = new Vector2();
 
         //Warm sampler with initial point
         newPoints.Add(initialPoint);
 
+        Debug.Log("Seeded points");
+        
         //Whilst we potentially still have points
         while (newPoints.Count > 0)
         {
@@ -29,23 +42,33 @@ public class PoissonDiscDistributionGenerator
             int index = Random.Range(0, newPoints.Count);
             Vector2 centre = newPoints[index];
             bool success = false;
-
+            
             //Try each point up to our limit
             for (int i = 0; i < MAX_ATTEMPTS; i++)
             {
+                Debug.Log("Trying point");
                 //Generate a point from a random direction
                 float rangle = Random.value * Mathf.PI * 2.0f;
 
                 Vector2 direction = new Vector2(Mathf.Sin(rangle), Mathf.Cos(rangle));
                 direction = centre + direction * Random.Range(spread, spread * 2.0f);
 
+                //If it's off the disc, return early
+                if (direction.sqrMagnitude > Mathf.Pow(maxRadius / 2, 2))
+                {
+                    continue;
+                }
+                
+                Vector2Int gridPos = ToGridPos(direction, cellSize, maxRadius, maxRadius / 2);
+
                 //Check position to see if it's alright
-                if (ValidPosition(direction, spread, maxRadius, points))
+                if (ValidPosition(gridPos, direction, spread, grid))
                 {
                     //Add to final points and add to points to check from
                     newPoints.Add(direction);
-
                     points.Add(direction);
+
+                    grid[gridPos.x, gridPos.y] = direction;
 
                     success = true;
                 }
@@ -58,35 +81,42 @@ public class PoissonDiscDistributionGenerator
             }
         }
 
-        return points;
+        bool done = true;
     }
 
     /// <summary>
     /// Checks the validity of a position, assuming that the centre of the disc is world zero.
     /// </summary>
-    public static bool ValidPosition(Vector3 point, float spread, float maxRadius, List<Vector3> points)
+    private static bool ValidPosition(Vector2Int point, Vector2 direction, float spread, Vector2[,] grid)
     {
         float sqSpread = spread * spread;
-        float sqRadius = maxRadius * maxRadius;
 
-        //If it's off the disc, return early
-        if (point.sqrMagnitude > sqRadius)
+        int xMin = Mathf.Max(point.x - 1, 0);
+        int yMin = Mathf.Max(point.y - 1, 0);
+        int xMax = Mathf.Min(point.x + 1, grid.GetLength(0)  - 1);
+        int yMax = Mathf.Min(point.y + 1, grid.GetLength(1) - 1);
+
+        for (int y = yMin; y < yMax; y++)
         {
-            return false;
-        }
-
-        //Otherwise, check every point to see if it's far enough away
-        for (int i = 0; i < points.Count; i++)
-        {
-            float sqDistance = (point - points[i]).sqrMagnitude;
-
-            //Point is too close
-            if (sqDistance < sqSpread)
-            { 
-                return false;
+            for (int x = xMin; x < xMax; x++)
+            {
+                Vector2 sample = grid[x, y];
+                if (sample != Vector2.zero)
+                {
+                    if ((sample - direction).sqrMagnitude < sqSpread)
+                        return false;
+                }
             }
         }
-
+        
         return true;
+    }
+
+    private static Vector2Int ToGridPos(Vector2 position, float cellSize, float maxRadius, float offset)
+    {
+        int xGrid = Mathf.FloorToInt((position.x + offset) / (maxRadius / cellSize));
+        int yGrid = Mathf.FloorToInt((position.y + offset) / (maxRadius / cellSize));
+        
+        return new Vector2Int(xGrid, yGrid);
     }
 }
